fd0fe0b6d3277f2b44d411e345d6c2e697da2319
martin <lenaerts.martin61@gmail.com>
2023-01-10 18:00:23 +0100

    create and test new route

--- src/lib/fastify.ts
+++ src/lib/fastify.ts
@@ -1,3 +1,36 @@
 import fastify from 'fastify'
+import * as CreateUserRequestBody from "../schemas/userRequest.json";
+import {CreateUserRequestBody as QuerystringSchemaInterface} from '../types/userRequest'
+import {dataSource} from "./datasource";
+import {User} from "../entities/user";
+import {SetPasswordDTO} from "../DTO/SetPasswordDTO";
 
-export const server = fastify()
+const server = fastify()
+
+server.post<{
+    Querystring: QuerystringSchemaInterface
+}>('/web-api/users', {
+    schema: {
+        querystring: CreateUserRequestBody
+    }
+}, async (request, reply) => {
+    const {email, firstname, lastname, password, passwordConfirmation} = request.query;
+
+    const repo = dataSource.getRepository(User);
+    const user = new User();
+
+    user.email = email;
+    user.firstname = firstname;
+    user.lastname = lastname;
+
+    await user.setPassword(new SetPasswordDTO(password, passwordConfirmation))
+
+    await repo.save(user)
+
+
+    reply.send("ok");
+})
+
+export {
+    server
+}
--- src/specs/routes/web-api/users-routes.spec.ts
+++ src/specs/routes/web-api/users-routes.spec.ts
@@ -1,10 +1,20 @@
 import {server} from '../../../lib/fastify'
+import {expect} from "chai";
 
 describe('/web-api/users', function () {
     describe('POST #create', function () {
         it('should register the user', async function () {
-            const response = await server.inject({url: `/web-api/users`, method: 'POST', payload: {}})
+            const response = await server.inject({
+                url: `/web-api/users`, method: 'POST', query: {
+                    firstname: "john",
+                    lastname: "doe",
+                    email: "john.doe@gmail.com",
+                    password: "verystrongpassword@123456789",
+                    passwordConfirmation: "verystrongpassword@123456789"
+                }
+            })
 
+            expect(response.statusCode).eq(200)
         })
     })
 })
