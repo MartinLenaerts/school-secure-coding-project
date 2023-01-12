d4872bccb81c496a75d96c42c9eee4c26eda0688
martin <lenaerts.martin61@gmail.com>
2023-01-12 20:07:08 +0100

    add tests when parameters are missing

--- src/specs/routes/web-api/users-routes.spec.ts
+++ src/specs/routes/web-api/users-routes.spec.ts
@@ -3,18 +3,48 @@ import {expect} from "chai";
 
 describe('/web-api/users', function () {
     describe('POST #create', function () {
+        let userQueryString = {
+            firstname: "john",
+            lastname: "doe",
+            email: "john.doe@gmail.com",
+            password: "verystrongpassword@123456789",
+            passwordConfirmation: "verystrongpassword@123456789"
+        };
+
+        beforeEach(function () {
+            userQueryString = {
+                firstname: "john",
+                lastname: "doe",
+                email: "john.doe@gmail.com",
+                password: "verystrongpassword@123456789",
+                passwordConfirmation: "verystrongpassword@123456789"
+            };
+        });
+
         it('should register the user', async function () {
             const response = await server.inject({
-                url: `/web-api/users`, method: 'POST', query: {
-                    firstname: "john",
-                    lastname: "doe",
-                    email: "john.doe@gmail.com",
-                    password: "verystrongpassword@123456789",
-                    passwordConfirmation: "verystrongpassword@123456789"
-                }
+                url: `/web-api/users`, method: 'POST', query: userQueryString
             });
 
+            console.log(response.body);
+
             expect(response.statusCode).eq(200);
+        });
+
+        describe('Missing parameters', function () {
+            for (const parameter of Object.keys(userQueryString)) {
+                it("should raise error if " + parameter + " is missing", async function () {
+                    delete userQueryString[parameter as keyof typeof userQueryString];
+
+                    const response = await server.inject({
+                        url: `/web-api/users`, method: 'POST', query: userQueryString
+                    });
+
+                    expect(response.statusCode).eq(400);
+                    expect(JSON.parse(response.body).message).eq("querystring must have required property '" + parameter + "'");
+                });
+            }
+
         });
     });
 });
