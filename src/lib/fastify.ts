826050d2ec4de172d9a6328f7be88989bac86dbe
martin <lenaerts.martin61@gmail.com>
2023-01-24 22:13:03 +0100

    add second hook

--- src/lib/fastify.ts
+++ src/lib/fastify.ts
@@ -1,13 +1,27 @@
-import fastify, {RouteOptions} from 'fastify';
+import fastify, {FastifyError, RouteOptions} from 'fastify';
 import {FASTIFY_LOGGING} from './dotenv';
 import {routes} from "../routes";
 
-export const server = fastify({logger: FASTIFY_LOGGING})
+export const server = fastify({
+    logger: FASTIFY_LOGGING,
+    ajv: {
+        customOptions: {
+            removeAdditional: false
+        }
+    }
+})
     .addHook('onRoute', assertsResponseSchemaPresenceHook)
+    .addHook('onRoute', assertsValidationSchemaPresenceHook)
     .register(routes, {prefix: '/'});
 
 export function assertsResponseSchemaPresenceHook(routeOptions: RouteOptions) {
     if (!routeOptions.schema?.response) {
         throw new Error(`No response schema found for route ${routeOptions.url}`);
+    }
+}
+
+export function assertsValidationSchemaPresenceHook(routeOptions: RouteOptions) {
+    if (!routeOptions.schema?.body && !routeOptions.schema?.params && !routeOptions.schema?.params) {
+        throw Error(`No query schema found for route ${routeOptions.url}`);
     }
 }
--- src/specs/routes/config/hook.spec.ts
+++ src/specs/routes/config/hook.spec.ts
@@ -1,10 +1,10 @@
 import fastify, {FastifyInstance} from "fastify";
-import {assertsResponseSchemaPresenceHook} from "../../../lib/fastify";
+import {assertsResponseSchemaPresenceHook, assertsValidationSchemaPresenceHook} from "../../../lib/fastify";
 import {expect} from "chai";
 
 describe('Fastify Hooks', function () {
     describe('onRoute hook', function () {
-        it("should raise error if route haven't schema", async function () {
+        it("should raise error if route haven't response schema", async function () {
             const server = fastify()
                 .addHook('onRoute', assertsResponseSchemaPresenceHook).register(async (fastify: FastifyInstance) => {
                     fastify.post("/test", () => true);
@@ -14,6 +14,19 @@ describe('Fastify Hooks', function () {
                 .eventually.be.rejected
                 .and.deep.include({
                     message: 'No response schema found for route /test',
+                });
+        });
+
+        it("should raise error if route haven't query schema", async function () {
+            const server = fastify()
+                .addHook('onRoute', assertsValidationSchemaPresenceHook).register(async (fastify: FastifyInstance) => {
+                    fastify.post("/test", () => true);
+                });
+
+            await expect(server).to
+                .eventually.be.rejected
+                .and.deep.include({
+                    message: 'No query schema found for route /test',
                 });
         });
     });
